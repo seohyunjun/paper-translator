879f339c8c273c89676c87a4587bb216783be37a
seohyunjun <bnmy6581@gmail.com>
2023-06-15 23:18:25 +0900

    add version

--- setup.py
+++ setup.py
@@ -2,16 +2,11 @@ from setuptools import setup
 
 
 setup(
-    name='paper-translator',
-    version='0.1.1',
-    description='Tranlsate paper into Korean',
-    author='seohyunjun',
-    author_email='bnmy6581@gmail.com',
-    url='https://github.com/seohyunjun/paper-translator',
-    install_requires=[
-        "langchain",
-        "openai",
-        "arxiv",
-        "pymupdf"
-    ],
+    name="paper-translator",
+    version="0.1.2",
+    description="Tranlsate paper into Korean",
+    author="seohyunjun",
+    author_email="bnmy6581@gmail.com",
+    url="https://github.com/seohyunjun/paper-translator",
+    install_requires=["langchain", "openai", "arxiv", "pymupdf"],
 )
