36e011d006505e1de5c46e3c45e55223a97b6086
seohyunjun <bnmy6581@gmail.com>
2023-06-18 22:46:55 +0900

    add outpufile filename

--- main.py
+++ main.py
@@ -17,11 +17,15 @@ def main(config):
 if __name__ == "__main__":
     # argeparse
     args = argparse.ArgumentParser()
-    args.add_argument("--pdf", type=str, default="https://arxiv.org/pdf/2305.11206.pdf")
+    args.add_argument("--pdf", type=str, default="https://arxiv.org/pdf/2305.10601.pdf")
     args.add_argument("--arxiv", type=str)
     args.add_argument("--verbose", type=bool, default=True)
     args.add_argument("--chunk_size", type=int, default=5000)
     args.add_argument("--outputfile", type=str)
-    args.add_argument("--model", type=str, default="gpt-3.5-turbo-0613")
+    args.add_argument("--model", type=str, default="gpt-3.5-turbo-16k")
+    
     config = args.parse_args()
+    
+    if config.pdf:
+        config.outputfile = config.pdf.split("/")[-1].split(".")[0] + ".md"   
     main(config)
