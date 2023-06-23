41da70f7f6d4f2630f5ca09092044fe41ef7dd73
seohyunjun <bnmy6581@gmail.com>
2023-06-24 00:11:13 +0900

    add v.0.1.3 url transfer markdown

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def main(config):
 if __name__ == "__main__":
     # argeparse
     args = argparse.ArgumentParser()
-    args.add_argument("--pdf", type=str, default="https://arxiv.org/pdf/2305.10601.pdf")
+    args.add_argument("--pdf", type=str)
     args.add_argument("--arxiv", type=str)
+    args.add_argument("--html", type=str, default='https://owasp.org/www-project-top-10-for-large-language-model-applications/descriptions/Prompt_Injection.html')
     args.add_argument("--verbose", type=bool, default=True)
     args.add_argument("--chunk_size", type=int, default=5000)
     args.add_argument("--outputfile", type=str)
@@ -27,5 +28,10 @@ if __name__ == "__main__":
     config = args.parse_args()
     
     if config.pdf:
-        config.outputfile = config.pdf.split("/")[-1].split(".")[0] + ".md"   
+        config.outputfile = config.pdf.split("/")[-1].split(".")[0] + ".md" 
+
+    
+    if config.html:
+        config.outputfile = config.html.split("/")[-1].split(".html")[0] + ".md" 
+    
     main(config)
