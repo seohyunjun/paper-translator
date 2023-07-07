8722210a631b6e6c7ec77abc6d5c6d82b0b5e0b6
seohyunjun <bnmy6581@gmail.com>
2023-07-07 21:49:43 +0900

    add gpt-4-8k model

--- main.py
+++ main.py
@@ -19,19 +19,20 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--pdf", type=str)
     args.add_argument("--arxiv", type=str)
-    args.add_argument("--html", type=str, default='https://owasp.org/www-project-top-10-for-large-language-model-applications/descriptions/Prompt_Injection.html')
+    args.add_argument("--html", type=str)
     args.add_argument("--verbose", type=bool, default=True)
     args.add_argument("--chunk_size", type=int, default=5000)
-    args.add_argument("--outputfile", type=str)
-    args.add_argument("--model", type=str, default="gpt-3.5-turbo-16k")
+    args.add_argument("--outputfile", type=str, default='test.md')
+    args.add_argument("--model", type=str, default="gpt-4-0613")
+    #args.add_argument("--model", type=str, default="gpt-3.5-turbo-16k")
     
     config = args.parse_args()
     
-    if config.pdf:
+    if (config.outputfile=='test.md') and config.pdf:
         config.outputfile = config.pdf.split("/")[-1].split(".")[0] + ".md" 
 
     
-    if config.html:
+    if (config.outputfile=='test.md') and config.html:
         config.outputfile = config.html.split("/")[-1].split(".html")[0] + ".md" 
     
     main(config)
